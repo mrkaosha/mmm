62f3ff717ff04830b0ad5bc81bb80ed3147fae5e
Joshua Kao <113385802+mrkaosha@users.noreply.github.com>
2023-01-25 07:14:07 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . d d d d d d d d d d d d . . 
+. d 4 4 4 d 5 5 5 5 5 d 4 4 d . 
+. d 4 4 4 d 5 5 5 5 5 d 4 4 d d 
+d 4 4 4 4 d 5 5 5 5 5 d 4 4 4 d 
+d 4 4 4 4 d d 5 5 5 d d 4 4 4 d 
+d 4 4 4 4 4 d 5 5 5 d 4 4 4 4 d 
+d 4 4 4 4 4 d d d d d 4 4 4 4 d 
+d 4 4 4 4 4 4 4 4 4 4 4 4 4 4 d 
+d 4 4 4 4 4 4 4 4 4 4 4 4 4 4 d 
+d d 4 4 4 4 4 4 4 4 4 4 4 4 d d 
+. d 4 4 4 4 4 4 4 4 4 4 4 4 d . 
+. d b 4 4 4 4 4 4 4 4 4 4 b d . 
+. . b 4 4 4 4 4 4 4 4 4 4 b . . 
+. . b b b b 4 4 4 4 b b b b . . 
+. . . . c c c c c c c c . . . . 
+. . . . . . c c c c . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
